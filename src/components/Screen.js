8fb0a7257e8f6330a9eecc749bd06109249f6037
stuartallen <allenstu@oregonstate.edu>
2023-02-19 22:54:45 -0800

    Barebones styling completed

---
+++ src/components/Screen.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Screen.css"
+
+const Screen = () => {
+    return (
+        <>
+            <div id="screenContainer">
+                Here's a screen
+                <canvas></canvas>
+            </div>
+        </>
+    )
+}
+
+export default Screen
