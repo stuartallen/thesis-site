927098ef505e4ed5b83d3ecd6ae32b358f8ec6f6
stuartallen <allenstu@oregonstate.edu>
2023-05-15 19:56:39 -0700

    Added color interpolation to EM Visualization

--- src/shaders/fragment/emVisFragment.js
+++ src/shaders/fragment/emVisFragment.js
@@ -9,24 +9,25 @@ varying float probability;
 const float LINES_FREQ = 0.005;
 const float LINES_WIDTH = 0.0000001;
 
+const vec3 color1 = vec3(1.0, 1.0, 0.0);
+const vec3 color2 = vec3(0.0, 1.0, 1.0);
+const vec3 color3 = vec3(1.0, 0.0, 1.0);
+
+float sigmoid(float x) {
+    return 1.0 / (1.0 + exp(-x));
+}
+
 void main() {
-	//	Original way
-	// vec4 color = mix(vec4(0.0, 0.0, 0.0, 0.7), vec4(0.7, 0.7, 1.0, 1.0), sqrt(sqrt(sqrt(probability))));
-
-	const vec3 color1 = vec3(1.0, 1.0, 0.0);
-	const vec3 color2 = vec3(0.0, 1.0, 1.0);
-	const vec3 color3 = vec3(1.0, 0.0, 1.0);
-
-	// vec3 vertexPos = vPosition.xyz / vPosition.w;
-
 	float dist1 = length(vPosition.xy - uMean1);
 	float dist2 = length(vPosition.xy - uMean2);
 	float dist3 = length(vPosition.xy - uMean3);
 
-	vec3 color = 	(dist1 <= dist2 && dist1 <= dist3) ? color1 :
-                	(dist2 <= dist1 && dist2 <= dist3) ? color2 :
-                	color3;
-	
+	float mixVal1 = sigmoid(probability) * (1.0 - (dist1 / (dist1 + dist2 + dist3)));
+	float mixVal2 = sigmoid(probability) * (1.0 - (dist2 / (dist1 + dist2 + dist3)));
+	float mixVal3 = sigmoid(probability) * (1.0 - (dist3 / (dist1 + dist2 + dist3)));
+
+	vec3 color = color1 * mixVal1 + color2 * mixVal2 + color3 * mixVal3;
+
 	float k = mod(probability, LINES_FREQ);
 	float sk = smoothstep(0.008/2.0 - LINES_WIDTH, 0.008/2.0 + LINES_WIDTH, k);
 
